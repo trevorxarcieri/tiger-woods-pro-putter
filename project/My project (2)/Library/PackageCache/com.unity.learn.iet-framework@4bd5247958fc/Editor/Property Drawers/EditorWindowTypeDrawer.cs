54ed813a86f02caf151a74725d6e2b881e38832e
trevorxarcieri <agent.steel.ta@gmail.com>
2025-06-04 12:25:23 -0400

    removed large files since it goes over my lfs limit.

--- project/My project (2)/Library/PackageCache/com.unity.learn.iet-framework@4bd5247958fc/Editor/Property Drawers/EditorWindowTypeDrawer.cs
+++
@@ -1,9 +0,0 @@
-using UnityEditor;
-
-namespace Unity.Tutorials.Core.Editor
-{
-    [CustomPropertyDrawer(typeof(EditorWindowType))]
-    class EditorWindowTypeDrawer : FlushChildrenDrawer
-    {
-    }
-}
